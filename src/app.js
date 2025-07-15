c6c031c191bbaeea9b3f8e53e6c7302d7c6c25bb
Siddesh s k <siddusiddesh551@gmail.com>
2025-07-15 16:38:36 +0530

    updated file

--- src/app.js
+++ src/app.js
@@ -1,13 +1,41 @@
 const express=require("express")
-const app=express()
-app.get("/",(req,res)=>{
-    res.send("first server is created")
-})
-app.get("/msg",(req,res)=>{
-    res.send("msg is created")
+const app=express();
+
+
+app.get("/user",(req,res,next)=>{
+   console.log("my first console")
+    next();
+},(req,res,next)=>{
+   console.log("my second console")
+    next();
+},(req,res,next)=>{
+   
+
+    console.log("my third console")
+    next();
+   
+},(req,res,next)=>{
+   
+
+    console.log("my fourth console")
+    next();
+   
+},(req,res,next)=>{
+  res.send("5th responce")
+   next();
+
+     console.log("my fifth console")
+     
+    
+},(req,res)=>{
+    console.log("hi");
+    // res.send("my 6th respnce")
+    
+    
+
 })
 
 
 app.listen(3000,()=>{
-    console.log("server is listening on port 3000")
+    console.log("server is listening on port 3000");
 })
